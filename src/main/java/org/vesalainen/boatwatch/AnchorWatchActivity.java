401e7b8c519920773809e7c9dbcd392721426258
Timo Vesalainen <timo.vesalainen@iki.fi>
2014-10-24 14:53:14 +0100

    Initial

---
+++ src/main/java/org/vesalainen/boatwatch/AnchorWatchActivity.java
@@ -0,0 +1,152 @@
+package org.vesalainen.boatwatch;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import org.ejml.data.DenseMatrix64F;
+import org.vesalainen.ui.AbstractView;
+
+public class AnchorWatchActivity extends Activity
+{
+    private LocationManager locationManager;
+    private AnchorView anchorView;
+
+    /**
+     * Called when the activity is first created.
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     * previously being shut down then this Bundle contains the data it most
+     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it
+     * is null.</b>
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        anchorView = new AnchorView(getBaseContext());
+        setContentView(anchorView);
+        
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, anchorView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(org.vesalainen.boatwatch.R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        locationManager.removeUpdates(anchorView);
+        super.onDestroy();
+    }
+
+    private class AnchorView extends View implements LocationListener
+    {
+        private Drawer drawer = new Drawer();
+        private DenseMatrix64F matrix = new DenseMatrix64F(10, 2);
+        private int index;
+        
+        public AnchorView(Context context)
+        {
+            super(context);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas)
+        {
+            super.onDraw(canvas);
+            drawer.setCanvas(canvas);
+            if (drawer.isReady())
+            {
+                drawer.drawPoints(matrix);
+            }
+        }
+
+        public void onLocationChanged(Location location)
+        {
+            double latitude = location.getLatitude();
+            double longitude = Math.cos(Math.toRadians(latitude))*location.getLongitude();
+            drawer.update(longitude, latitude);
+            matrix.set(index, 0, longitude);
+            matrix.set(index, 1, latitude);
+            index++;
+            if (index == matrix.numRows)
+            {
+                invalidate();
+                index = 0;
+            }
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras)
+        {
+        }
+
+        public void onProviderEnabled(String provider)
+        {
+        }
+
+        public void onProviderDisabled(String provider)
+        {
+        }
+        
+    }
+    private class Drawer extends AbstractView
+    {
+        private Paint paint;
+        private Canvas canvas;
+
+        public Drawer()
+        {
+            super();
+            paint = new Paint();
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setAntiAlias(true);
+        }
+
+        private void setCanvas(Canvas canvas)
+        {
+            this.canvas = canvas;
+            setScreen(canvas.getWidth(), canvas.getHeight());
+        }
+
+        private void drawCircle(float x, float y, float r)
+        {
+            float sx = (float) translateX(x);
+            float sy = (float) translateY(y);
+            float sr = (float) scale(r);
+            int w = canvas.getWidth();
+            int h = canvas.getHeight();
+            String text = "x="+x+" y="+y+" sx="+sx+" sy="+sy+" sr="+sr+" w="+w+" h="+h+" xMax="+xMax+" scale="+scale;
+            canvas.drawText(text, 5, 20, paint);
+            canvas.drawCircle((float) translateX(x), (float) translateY(y), (float) scale(r), paint);
+        }
+
+        private void drawPoints(DenseMatrix64F matrix)
+        {
+            for (int row = 0;row<matrix.numRows;row++)
+            {
+                double x = matrix.get(row, 0);
+                double y = matrix.get(row, 1);
+                float tx = (float) translateX(x);
+                float ty = (float) translateY(y);
+                String text = "x="+x+" y="+y+" tx="+tx+" ty="+ty;
+                canvas.drawText(text, 5, 20*(row+1), paint);
+                canvas.drawPoint(tx, ty, paint);
+            }
+        }
+        
+    }
+}
