3b27e80abf76661c0931b1088dd8f0587aa3d415
Timo Vesalainen <timo.vesalainen@iki.fi>
2014-11-29 17:42:49 +0000

    Dev

--- src/main/java/org/vesalainen/boatwatch/AnchorWatchActivity.java
+++ src/main/java/org/vesalainen/boatwatch/AnchorWatchActivity.java
@@ -1,38 +1,61 @@
 package org.vesalainen.boatwatch;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.Point;
+import android.graphics.RectF;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.ArcShape;
 import android.location.Location;
 import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
-import android.util.Pair;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
 import java.util.ArrayList;
 import java.util.List;
 import org.ejml.data.DenseMatrix64F;
+import org.vesalainen.boatwatch.AnchorWatchService.AnchorWatchBinder;
+import org.vesalainen.math.Circle;
+import org.vesalainen.math.CircleFitter;
+import org.vesalainen.math.ConvexPolygon;
+import org.vesalainen.math.Sector;
+import org.vesalainen.navi.AnchorWatch.Watcher;
 import org.vesalainen.ui.AbstractView;
-import org.vesalainen.util.math.CircleFitter;
-import org.vesalainen.util.math.ConvexPolygon;
-import org.vesalainen.util.math.Polygon;
+import org.vesalainen.ui.MouldableSector;
 
 public class AnchorWatchActivity extends Activity
 {
-    private static final double Cable = 1.0/600.0;
-    private static final double DegreeToMeters = 36.0/4000000.0;
-    private static final double MaxRadius = 50*DegreeToMeters;
-    private static final double MinRadius = 30*DegreeToMeters;
-    private static final int Size = 10;
-
-    private LocationManager locationManager;
+    private static final String AnchorWatch = "AnchorWatch";
     private AnchorView anchorView;
+    private AnchorWatchBinder binder;
+    private boolean bound;
 
+    private ServiceConnection connection = new ServiceConnection()
+    {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service)
+        {
+            Log.d(AnchorWatch, "onServiceConnected "+anchorView);
+            binder = (AnchorWatchBinder) service;
+            binder.addWatcher(anchorView);
+            bound = true;
+        }
+        @Override
+        public void onServiceDisconnected(ComponentName name)
+        {
+            binder.removeWatcher(anchorView);
+            bound = false;
+        }
+           
+    };
+    private Intent serviceIntent;
     /**
      * Called when the activity is first created.
      *
@@ -45,11 +68,34 @@ public class AnchorWatchActivity extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        Log.d(AnchorWatch, "onCreate");
+        
         anchorView = new AnchorView(getBaseContext());
         setContentView(anchorView);
+
+        serviceIntent = new Intent(this, AnchorWatchService.class);
+        startService(serviceIntent);
         
-        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 1, anchorView);
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        Log.d(AnchorWatch, "onStart");
+        Intent intent = new Intent(this, AnchorWatchService.class);
+        bindService(intent, connection, Context.BIND_AUTO_CREATE);        
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        if (bound)
+        {
+            unbindService(connection);
+            binder.removeWatcher(anchorView);
+        }
     }
 
     @Override
@@ -63,27 +109,23 @@ public class AnchorWatchActivity extends Activity
     @Override
     protected void onDestroy()
     {
-        locationManager.removeUpdates(anchorView);
+        if (serviceIntent != null)
+        {
+            stopService(serviceIntent);
+        }
         super.onDestroy();
     }
 
-    private class AnchorView extends View implements LocationListener
+    private class AnchorView extends View implements Watcher
     {
+
         private final Drawer drawer = new Drawer();
-        private final DenseMatrix64F points = new DenseMatrix64F(Size, 2);
-        private final DenseMatrix64F center = new DenseMatrix64F(2, 1);
-        private final DenseMatrix64F meanCenter = new DenseMatrix64F(2, 1);
-        private int index;
-        private CircleFitter circleFitter;
-        private List<P> centers = new ArrayList<>();
-        private double cx;
-        private double cy;
-        private double delta = 0.0;
-        private double finalCost = 0;
-        private ConvexPolygon polygon = new ConvexPolygon();
-        private int locCount = 0;
-        private boolean anchorSet = false;
-        
+        private double lastX;
+        private double lastY;
+        private ConvexPolygon area;
+        private Circle estimated;
+        private MouldableSector safe;
+
         public AnchorView(Context context)
         {
             super(context);
@@ -95,53 +137,19 @@ public class AnchorWatchActivity extends Activity
             super.onDraw(canvas);
             canvas.drawARGB(255, 0, 0, 0);
             drawer.setCanvas(canvas);
-            drawer.drawText(0, "Count="+locCount);
-            drawer.drawPoints(points, index);
-            if (circleFitter != null)
+            drawer.drawPoint(lastX, lastX);
+            if (area != null)
             {
-                drawer.drawCircle(circleFitter);
+                drawer.drawPolygon(area);
             }
-        }
-
-        public void onLocationChanged(Location location)
-        {
-            locCount++;
-            if  (locCount < 3)
+            if (estimated != null)
             {
-                return;
+                drawer.drawCircle(estimated);
             }
-            double latitude = location.getLatitude();
-            double longitude = Math.cos(Math.toRadians(latitude))*location.getLongitude();
-            drawer.update(longitude, latitude);
-            points.set(index, 0, longitude);
-            points.set(index, 1, latitude);
-            index++;
-            if (index == points.numRows)
+            if (safe != null)
             {
-                for (int ii=0;ii<points.numRows;ii++)
-                {
-                    Log.d("AnchorWatch", "("+points.data[2*ii]+", "+points.data[2*ii+1]+")");
-                }
-                polygon.updateConvexPolygon(points);
-                points.reshape(polygon.points.numRows, 2);
-                points.set(polygon.points);
-                CircleFitter.meanCenter(points, meanCenter);
-                CircleFitter.limitDistance(meanCenter, center, MinRadius, MaxRadius);
-                if (circleFitter == null)
-                {
-                    double radius = CircleFitter.initialCenter(points, center);
-                    circleFitter = new CircleFitter(center);
-                }
-                if (anchorSet)
-                {
-                    CircleFitter.filterInnerPoints(points, center, points.numRows/3, 0.8);
-                }
-                circleFitter.fit(points);
-                finalCost = circleFitter.getLevenbergMarquardt().getFinalCost();
-                index = points.numRows;
-                points.reshape(points.numRows+Size, 2, true);
+                drawer.drawSector(safe);
             }
-            postInvalidate();
         }
 
         @Override
@@ -151,36 +159,6 @@ public class AnchorWatchActivity extends Activity
             switch (action)
             {
                 case MotionEvent.ACTION_DOWN:
-                    for (int ii=0;ii<points.numRows;ii++)
-                    {
-                        Log.d("AnchorWatch", "("+points.data[2*ii]+", "+points.data[2*ii+1]+")");
-                    }
-                    float x = event.getX();
-                    float y = event.getY();
-                    center.data[0] = drawer.fromScreenX(x);
-                    center.data[1] = drawer.fromScreenY(y);
-                    Log.d("AnchorWatch", "("+x+", "+y+") -> ("+center.data[0]+", "+center.data[1]+")");
-                    points.reshape(index, 2, true);
-                    CircleFitter.filterInnerPoints(points, center, points.numRows/3, 0.8);
-                    index = points.numRows;
-                    points.reshape(points.numRows+Size, 2, true);
-                    CircleFitter.meanCenter(points, meanCenter);
-                    Log.d("AnchorWatch", "meanCenter = ("+meanCenter.data[0]+", "+meanCenter.data[1]+")");
-                    CircleFitter.limitDistance(meanCenter, center, MinRadius, MaxRadius);
-                    Log.d("AnchorWatch", "center = ("+center.data[0]+", "+center.data[1]+")");
-                    if (circleFitter == null)
-                    {
-                        circleFitter = new CircleFitter(center);
-                    }
-                    else
-                    {
-                        circleFitter.getCenter().set(center);
-                    }
-                    circleFitter.fit(points);
-                    Log.d("AnchorWatch", "center = ("+center.data[0]+", "+center.data[1]+")");
-                    finalCost = circleFitter.getLevenbergMarquardt().getFinalCost();
-                    anchorSet = true;
-                    postInvalidate();
                     break;
             }
             return true;
@@ -198,19 +176,58 @@ public class AnchorWatchActivity extends Activity
         {
         }
 
-        private double distance(double cx, double cy, double xx, double yy)
+        @Override
+        public void alarm(double distance)
         {
-            double dx = cx-xx;
-            double dy = cy-yy;
-            return Math.sqrt(dx*dx+dy*dy);
         }
-        
+
+        @Override
+        public void location(double x, double y)
+        {
+            lastX = x;
+            lastY = y;
+            drawer.updatePoint(x, y);
+            invalidate();
+        }
+
+        @Override
+        public void area(ConvexPolygon area)
+        {
+            drawer.updatePolygon(area);
+            this.area = area;
+            invalidate();
+        }
+
+        @Override
+        public void outer(DenseMatrix64F path)
+        {
+        }
+
+        @Override
+        public void estimated(Circle estimated)
+        {
+            drawer.updateCircle(estimated);
+            this.estimated = estimated;
+            invalidate();
+        }
+
+        @Override
+        public void safeSector(MouldableSector safe)
+        {
+            drawer.updateCircle(safe);
+            this.safe = safe;
+            invalidate();
+        }
+
     }
+
     private class Drawer extends AbstractView
     {
-        private Paint white;
-        private Paint black;
-        private Paint red;
+
+        private final Paint white;
+        private final Paint black;
+        private final Paint red;
+        private final Paint green;
         private Canvas canvas;
 
         public Drawer()
@@ -224,6 +241,9 @@ public class AnchorWatchActivity extends Activity
             red = new Paint();
             red.setStyle(Paint.Style.STROKE);
             red.setARGB(255, 255, 0, 0);
+            green = new Paint();
+            green.setStyle(Paint.Style.STROKE);
+            green.setARGB(255, 0, 255, 0);
         }
 
         private void setCanvas(Canvas canvas)
@@ -232,93 +252,67 @@ public class AnchorWatchActivity extends Activity
             setScreen(canvas.getWidth(), canvas.getHeight());
         }
 
-        private void drawCircle(float x, float y, float r)
+        private void drawPoint(double x, double y)
         {
-            float sx = (float) toScreenX(x);
-            float sy = (float) toScreenY(y);
-            float sr = (float) scale(r);
-            int w = canvas.getWidth();
-            int h = canvas.getHeight();
-            String text = "x="+x+" y="+y+" sx="+sx+" sy="+sy+" sr="+sr+" w="+w+" h="+h+" xMax="+xMax+" scale="+scale;
-            canvas.drawText(text, 5, 40, red);
-            canvas.drawCircle((float) toScreenX(x), (float) toScreenY(y), (float) scale(r), white);
+            float tx = (float) toScreenX(x);
+            float ty = (float) toScreenY(y);
+            canvas.drawCircle(tx, ty, 5, white);
         }
-
-        private void drawPoints(DenseMatrix64F matrix)
+        private void drawCircle(Circle circle)
         {
-            drawPoints(matrix, matrix.numRows);
+            float sx = (float) toScreenX(circle.getX());
+            float sy = (float) toScreenY(circle.getY());
+            float sr = (float) scale(circle.getRadius());
+            canvas.drawCircle(sx, sy, sr, green);
         }
-        private void drawPoints(DenseMatrix64F matrix, int count)
+        private void drawSector(Sector sector)
         {
-            for (int row = 0;row<count;row++)
-            {
-                double x = matrix.get(row, 0);
-                double y = matrix.get(row, 1);
-                float tx = (float) toScreenX(x);
-                float ty = (float) toScreenY(y);
-                if (count == matrix.numRows)
-                {
-                    Log.d("AnchorWatch", x+","+y+","+tx+","+ty+",");
-                }
-                //String text = "x="+x+" y="+y+" tx="+tx+" ty="+ty;
-                //canvas.drawText(text, 5, 20*(row+2), paint);
-                canvas.drawCircle(tx, ty, 10, white);
-            }
+            float x = (float) toScreenX(sector.getX());
+            float y = (float) toScreenY(sector.getY());
+            float r = (float) scale(sector.getRadius());
+            RectF rect = new RectF(
+                    x-r, 
+                    y-r, 
+                    x+r, 
+                    y+r
+            );
+            canvas.drawArc(
+                    rect, 
+                    (float)Math.toDegrees(sector.getRightAngle()), 
+                    (float)Math.toDegrees(sector.getLeftAngle()),
+                    true, 
+                    red);
         }
-
-        private void drawCircle(CircleFitter circleFitter)
-        {
-            DenseMatrix64F center = circleFitter.getCenter();
-            float x = (float)center.data[0];
-            float y = (float)center.data[1];
-            float radius = (float) circleFitter.getRadius();
-            float sx = (float) toScreenX(x);
-            float sy = (float) toScreenY(y);
-            float sr = (float) scale(radius);
-            update(x, y, radius);
-            String fmt = String.format("center (%.6f, %.6f) r=%.0f", x, y, radius/DegreeToMeters);
-            drawText(4, fmt);
-            drawText(5, "Init cost="+circleFitter.getLevenbergMarquardt().getInitialCost());
-            drawText(6, "Final cost="+circleFitter.getLevenbergMarquardt().getFinalCost());
-            canvas.drawCircle(sx, sy, sr, red);
-            canvas.drawCircle(sx, sy, 6, red);
-        }
-
         private void drawText(String text, int x, int y)
         {
             canvas.drawText(text, x, y, white);
         }
-        
+
         private void drawText(int line, String text)
         {
-            canvas.drawText(text, 5, 20*(line+1), white);
+            canvas.drawText(text, 5, 20 * (line + 1), white);
         }
 
-        private void drawCenters(List<P> centers)
+        private void drawPolygon(ConvexPolygon area)
         {
-            for (P p : centers)
+            DenseMatrix64F m = area.points;
+            double[] d = m.data;
+            int rows = m.numRows;
+            if (rows >= 2)
             {
-                float tx = (float) toScreenX(p.x);
-                float ty = (float) toScreenY(p.y);
-                canvas.drawCircle(tx, ty, 10, red);
+                int x1 = (int) toScreenX(d[2*(rows-1)]);
+                int y1 = (int) toScreenY(d[2*(rows-1)+1]);
+                for (int r=0;r<rows;r++)
+                {
+                    int x2 = (int) toScreenX(d[2*r]);
+                    int y2 = (int) toScreenY(d[2*r+1]);
+                    canvas.drawLine(x1, y1, x2, y2, white);
+                    x1 = x2;
+                    y1 = y2;
+                }
             }
         }
-    }        
-    private static class P
-    {
-        private final double x;
-        private final double y;
 
-        public P(double x, double y)
-        {
-            this.x = x;
-            this.y = y;
-        }
-
-        public P(double[] data)
-        {
-            this.x = data[0];
-            this.y = data[1];
-        }
     }
+
 }
---
+++ src/main/java/org/vesalainen/boatwatch/AnchorWatchService.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (C) 2014 Timo Vesalainen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.vesalainen.boatwatch;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Binder;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.widget.Toast;
+import org.vesalainen.navi.AnchorWatch;
+import org.vesalainen.navi.AnchorWatch.Watcher;
+
+/**
+ *
+ * @author Timo Vesalainen
+ */
+public class AnchorWatchService extends Service implements LocationListener
+{
+    private final AnchorWatch watch = new AnchorWatch();
+    private LocationManager locationManager;
+    private final IBinder binder = new AnchorWatchBinder();
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
+        Toast.makeText(this, "onStartCommand", Toast.LENGTH_SHORT).show();
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 1, this);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent)
+    {
+        Toast.makeText(this, "onBind", Toast.LENGTH_SHORT).show();
+        return binder;
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
+        locationManager.removeUpdates(this);
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLocationChanged(Location location)
+    {
+        watch.update(location.getLongitude(), location.getLatitude());
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras)
+    {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider)
+    {
+    }
+
+    @Override
+    public void onProviderDisabled(String provider)
+    {
+    }
+    
+    public class AnchorWatchBinder extends Binder
+    {
+        void addWatcher(Watcher watcher)
+        {
+            watch.addWatcher(watcher);
+        }
+        void removeWatcher(Watcher watcher)
+        {
+            watch.removeWatcher(watcher);
+        }
+    }
+}
